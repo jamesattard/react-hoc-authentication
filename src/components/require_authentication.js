843c5624b4f595edc929f4cae3a273e1f7d1e210
James J Attard <info@jamesattard.com>
2017-10-13 15:56:12 +0200

    hooked authentication router logic

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+  
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.props.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      console.log(nextProps)
+      if (!nextProps.authenticated) {
+        this.props.router.push('/');
+      }
+    }
+
     render() {
       return <ComposedComponent {...this.props} />
     }
   }
 
-  return Authentication;
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(withRouter(Authentication));
 }
